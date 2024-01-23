73e21a739117877a03df4cb2d8a5c28775eac617
Theo / Taeyoon Kang <kang3603a@gmail.com>
2024-01-23 14:53:57 +0900

    fix: build.gradle 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,58 @@
 plugins {
-  kotlin("jvm") version "1.9.22"
-  id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("maven-publish")
 }
 
-group = "dev.tykan"
-version = "1.0-SNAPSHOT"
 
-repositories {
-  mavenCentral()
+allprojects{
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    group = "dev.tykan"
+    version = "1.0.3"
+    
+    apply(plugin = "kotlin")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    
+    dependencies {
+        val exposedVersion = "0.44.1"
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-batch")
+        implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+        implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.batch:spring-batch-test")
+    }
+    
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "1.8"
+            }
+            dependsOn(processResources) // kotlin 에서 ConfigurationProperties
+        }
+        
+        
+        compileTestKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "1.8"
+            }
+        }
+    }
+}
+
+project("spring-batch-exposed-reader") {
+    dependencies {
+    
+    }
 }
